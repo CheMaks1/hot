ab51d876a486aa4134223ab58cb3108768aa5a1a
Maksim Andreev <chemaks@mail.ru>
2024-10-20 13:41:19 +0600

    rev.3.test-roll-back

--- conftest.py
+++ conftest.py
@@ -2,41 +2,17 @@
 import pytest
 from pages.advertiser_login import AdvertiserLogin
 from pages.create_promo_page import PromoPage
-from playwright.sync_api import sync_playwright
-
-
-@pytest.fixture(scope="session")
-def playwright():
-    with sync_playwright() as p:
-        yield p
-
-
-@pytest.fixture(scope="session")
-def browser(playwright):
-    browser = playwright.chromium.launch(headless=True)
-    yield browser
-    browser.close()
-
-
-@pytest.fixture(scope="function")
-def context(browser):
-    context = browser.new_context()
-    yield context
-    context.close()
 
 
 @pytest.fixture()
-def sign_in_page(context):
-    page = context.new_page()
-    yield AdvertiserLogin(page)
-    page.close()
+def sign_in_page(page):
+    return AdvertiserLogin(page)
 
 
 @pytest.fixture()
-def create_promo_page(context):
-    page = context.new_page()
-    yield PromoPage(page)
-    page.close()
+def create_promo_page(page):
+    return PromoPage(page)
+
 
 # @pytest.fixture()
 # def page(context: BrowserContext, playwright):
